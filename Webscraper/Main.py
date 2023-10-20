75d3694a1b01cf15e1d8c56204a2992e00de7683
Pauls <paulskristaps.l@gmail.com>
2023-10-20 17:42:05 +0000

    pievienoti komentāri kodam

--- Webscraper/Main.py
+++ Webscraper/Main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Pievieno nepieciešamos headerus, lai varētu piekļūt mājaslapas serverim
 headers = { 
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'
 }
@@ -9,8 +10,12 @@ headers = {
 
 page = "https://www.amazon.com/gp/bestsellers?&linkCode=sl2&tag=pandatech02-20&linkId=a45d9b9261d98350ed2ae157280272e1&language=en_US&ref_=as_li_ss_tl"
 response = requests.get(page, headers=headers)
+
+#ja savienojums izdevies, saglabāt savienotās mājaslapas html, kā string failu
 if response.status_code == 200:
     html = response.text
+
+	#Izmantojam lxml parseri, lai strādātu ar html, tas ir labāks nekā  default
     doc = BeautifulSoup(html, "lxml")
 
     rows = doc.findAll('a', class_='a-link-normal')
