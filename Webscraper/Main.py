8032f965ca0d68b9f6899ea9ca9f2c1a1341e3b8
Pauls Kristaps <paulskristaps@Guntarss-MacBook-Pro.local>
2023-10-21 12:10:35 +0300

    Implementēta failu logošana, datubāzes

--- Webscraper/Main.py
+++ Webscraper/Main.py
@@ -1,17 +1,34 @@
 import sqlite3
+import logging
+import logging.config
+import yaml
+
+with open("Webscraper/logging_config.yaml", "r") as config_file:
+    config = yaml.safe_load(config_file)
+
+logging.config.dictConfig(config)
+
+logger = logging.getLogger("my_logger")
 
 connection = sqlite3.connect("Webscraper/database.db")
 cursor = connection.cursor()
 
 #Paņem visu produktu informāciju no datubāzes
+logger.debug("Selecting data from DB...")
 cursor.execute("SELECT * FROM products")
 rows = cursor.fetchall()
 
-# Izprintē informāciju
+# Loggo informāciju
 i = 0
 for row in rows:
     i+= 1
-    print("Product #" +str(i)+ " -- " + row[0] + " has a rating of " + str(row[1]) + ", and it costs $" + str(row[2]) +"\n")
+    logger.info("Product #" +str(i)+ " -- " + row[0] + " has a rating of " + str(row[1]) + ", and it costs $" + str(row[2]) +"\n")
+
+logger.debug("COMPLETED!")
+
+with open("Webscraper/logfile.log", "r") as printout:
+    for row in printout:
+        print(row)
 
 connection.commit()
 connection.close()
